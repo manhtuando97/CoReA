b5a23f7cb2c08f49050936c21d3be09356f00bcb
Manh Tuan Do <42369265+manhtuando97@users.noreply.github.com>
2022-10-14 00:58:29 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -22,9 +22,9 @@ def main(args):
 
     availability, order, index_start_deg = h_graph.anchor_availability(scheme = args.tie)
     candidates = h_graph.fill_candidate(availability, order, index_start_deg, degeneracy_scheme = args.degeneracy, scheme = args.scheme)
-    augmented = h_graph.rank_select(candidates, scheme = args.rank)
+    augmented = h_graph.rank_select(candidates, scheme = args.rank, budget = args.budget)
 
-    f = open('{}/Hypergraph/Augmentation/{}-{}-{}-{}-{}.txt'.format(parent_directory, args.dataset, args.tie, str(args.degeneracy), args.scheme, args.rank), 'w')
+    f = open('{}/Hypergraph/Augmentation/{}-{}-{}-{}-{}-{}.txt'.format(parent_directory, args.dataset, args.tie, str(args.degeneracy), args.scheme, args.rank, str(args.budget)), 'w')
     for hyperedge in augmented:
         string = ' '.join([str(x) for x in hyperedge])
         f.write(string +'\n')
@@ -40,13 +40,14 @@ if __name__ == '__main__':
     parser.add_argument('degeneracy', type=int, help='Select the degeneracy enforcementin Step 1-2: 0/1')
     parser.add_argument('scheme', type=str, help='Select the selection scheme in Step 1-2: random/IS')
     parser.add_argument('rank', type=str, help='Select the ranking scheme in Step 2: random/mrkc/main')
+    parser.add_argument('budget', type=int, help='Budget: number of hyperedges to augment')
 
     args = parser.parse_args()
-    datasets_infor = {'coauth-DBLP': "coauth-DBLP", 'coauth-MAG-Geology': "coauth-MAG-Geology", 'coauth-MAG-History': "coauth-MAG-History",
-                      'congress-bills': "congress-bills", 'contact-high-school': "contact-high-school", 'contact-primary-school': "contact-primary-school",
-                      'DAWN': "DAWN", 'email-Enron': "email-Enron", 'email-Eu': "email-Eu", 'NDC-classes': "NDC-classes",
-                      'NDC-substances': "NDC-substances", 'tags-math': "tags-math", 'tags-ask-ubuntu': "tags-ask-ubuntu", 'tags-stack-overflow': "tags-stack-overflow",
-                      'threads-ask-ubuntu': "threads-ask-ubuntu", 'threads-math': "threads-math", 'threads-stack-overflow': "threads-stack-overflow"}
+    datasets_infor = {'coauth-MAG-Geology': "coauth-MAG-Geology", 'coauth-MAG-History': "coauth-MAG-History",
+                      'contact-high-school': "contact-high-school", 'contact-primary-school': "contact-primary-school",
+                      'email-Enron': "email-Enron", 'email-Eu': "email-Eu", 'NDC-classes': "NDC-classes",
+                      'NDC-substances': "NDC-substances", 
+                      'threads-ask-ubuntu': "threads-ask-ubuntu", 'threads-math': "threads-math"}
 
 
     if args.dataset in datasets_infor:
